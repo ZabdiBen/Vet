5fbeef0c34d7ce4b68dc9956069086bed5f84adc
ZabdiBen <montaeze@gmail.com>
2022-10-12 13:14:30 +0300

    Dandole al curso

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -2,7 +2,15 @@ import Veterinario from "../models/Veterinario.js"
 
 const registrar = async (req, res) => {
     
-    const { email, password, nombre } = req.body
+    const { email } = req.body
+
+    //prevenir usuarios duplicados
+    const existeUsuario = await Veterinario.findOne({email})
+
+    if(existeUsuario){
+        const err = new Error('The user exist')
+        return res.status(400).json({msg: err.message})
+    }
 
     try {
         //Guardar un nuevo veterinario
@@ -20,4 +28,26 @@ const perfil = (req, res) => {
     res.json({msg:'Hola veterinario'})
 }
 
-export { registrar, perfil}
+const confirmar = async (req, res) => {
+    const {token} = req.params;
+
+    const usuarioConfirmar = await Veterinario.findOne({token}) //Es objecto pero en la sintaxis moderna de javascript se puede escribir solo la llave
+
+    if(!usuarioConfirmar){
+        const error = new Error('token no valido')
+        return res.status(404).json({msg: error.message})
+    }
+
+    try {
+        usuarioConfirmar.token = null
+        usuarioConfirmar.confirmado = true
+        await usuarioConfirmar.save()
+        res.json({msg:'Usuario confirmado correctamente ...'})
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+export { registrar, perfil, confirmar}
